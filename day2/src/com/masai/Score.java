72f933967264609d0855dccf37104ecb9f5da30c
Gaurav Jaiswal <101566310+Aryan-2-2@users.noreply.github.com>
2022-05-18 21:13:54 +0530

    assignment

---
+++ day2/src/com/masai/Score.java
@@ -0,0 +1,17 @@
+package com.masai;
+
+public class Score {
+
+	 public static void main(String[] args) {
+		int ones = 1;
+		int twos = 5;
+		int threes = 2;
+		int fours = 6;
+		int sixs = 5;
+		
+		
+int totalScore = (ones)+(twos*2)+(threes*3)+(fours*4)+(sixs*6);
+ 
+ System.out.println(totalScore);
+	}
+}
---
+++ day2/src/com/masai/SumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+public class SumOfNaturalNumbers {
+	
+    public static void main(String[] args) {
+	
+   
+    	
+    	int N = 5;
+    	
+    	int output = N*(N+1)/2;
+    	
+    	System.out.println(output);
+}
+}
